0c8f16aa4caa679fff3466a324c799d16ae97743
Jim Blaney <jblaney@esri.com>
2014-02-18 18:27:40 -0800

    tidyness

--- src/app/widgets/TripIn/Widget.js
+++ src/app/widgets/TripIn/Widget.js
@@ -1,25 +1,29 @@
 define([
-  "dojo/_base/declare",
-  "dijit/_WidgetsInTemplateMixin",
-  "jimu/BaseWidget",
-  "jimu/dijit/TabContainer",
-  "jimu/dijit/List",
-  "jimu/utils",
+  'dojo/_base/declare',
+  'dijit/_WidgetsInTemplateMixin',
+  'jimu/BaseWidget',
+  'jimu/dijit/TabContainer',
+  'jimu/dijit/List',
+  'jimu/utils',
   './EventEditor'
 ], function(declare,
   _WidgetsInTemplateMixin,
-  BaseWidget, TabContainer, List, utils,
+  BaseWidget, 
+  TabContainer, 
+  List, 
+  utils,
   EventEditor) {
   return declare([BaseWidget, _WidgetsInTemplateMixin], {
-    name: "TripIn",
-    baseClass: "tripin-workflow-parent",
+    name: 'TripIn',
+    baseClass: 'tripin-workflow-parent',
+    eventEditor: null,
     postCreate: function() {
       this.inherited(arguments);
       this.eventEditor = new EventEditor();
     },
     startup: function() {
       this.inherited(arguments);
-      console.log("WorkflowParent::startup()");
+      console.log('TripIn::startup()');
     },
     onOpen: function() {
       this.inherited(arguments);
@@ -27,7 +31,7 @@ define([
       this.eventEditor.placeAt(this.containerNode);
     },
     onClose: function() {
-      console.log("WorkflowParent::onClose()");
+      console.log('TripIn::onClose()');
     }
   });
 });
