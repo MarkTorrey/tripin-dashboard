f83ec1b83e8fcb86b3bb58e3bd56c0fe08b1bc2f
Tom Wayson <tom@tomwayson.com>
2014-02-18 19:27:56 -0800

    ugly event editor form

--- src/app/widgets/TripIn/EventEditor.js
+++ src/app/widgets/TripIn/EventEditor.js
@@ -3,32 +3,39 @@ define([
 
   'dijit/_WidgetBase',
   'dijit/_TemplatedMixin',
-  // "dijit/_WidgetsInTemplateMixin",
+  "dijit/_WidgetsInTemplateMixin",
 
-  'dojo/text!./EventEditor.html'
+  'dojo/text!./EventEditor.html',
 
+  // used in template
+  'dijit/form/Form',
+  'dijit/form/DateTextBox',
+  'dijit/form/TimeTextBox'
 ], function(
   declare,
-  _WidgetBase, _TemplatedMixin,
+  _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
   template
 ) {
-  return declare([_WidgetBase, _TemplatedMixin], {
+  return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
 
-    //please note that this property is be set by the framework when widget is loaded.
     templateString: template,
 
     baseClass: 'tripin-event-editor',
 
     name: 'EventEditor',
 
+    title: 'Event Editor',
+
+    _setTitleAttr: { node: "titleNode", type: "innerHTML" },
+
     postCreate: function() {
       this.inherited(arguments);
-      console.log('postCreate');
+      console.log('EventEditor::postCreate');
     },
 
     startup: function() {
       this.inherited(arguments);
-      console.log('startup');
+      console.log('EventEditor::startup');
     }
   });
 });
