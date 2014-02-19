695ffc08273d914d7f72f1e5ac23cf3896887ca7
Jim Blaney <jblaney@esri.com>
2014-02-18 17:45:05 -0800

    bound application to WorkflowParent widget
    for now, it's in the top-right corner of the application (the button
    with a "broken" image)

--- src/app/widgets/WorkflowParent/Widget.js
+++ src/app/widgets/WorkflowParent/Widget.js
@@ -5,7 +5,7 @@ define([
   "jimu/dijit/TabContainer",
   "jimu/dijit/List",
   "jimu/utils"
-]), function(declare, _WidgetsInTemplateMixin, BaseWidget, TabContainer, List, utils) {
+], function(declare, _WidgetsInTemplateMixin, BaseWidget, TabContainer, List, utils) {
   return declare([BaseWidget, _WidgetsInTemplateMixin], {
     name: "TripIn",
     baseClass: "tripin-workflow-parent",
